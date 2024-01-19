18eb9b5c027a61e03cc13caf32826edb0827d49c
Habeeb <skhabeebhussain91@.com>
2024-01-19 15:41:35 +0530

    Assignment completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { Component, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import HigherOrderFunction from './components/hof/HigherOrderFunction'
+
+
+
+function App() {
+
+  return (
+
+      <>
+  <HigherOrderFunction />
+      </>
+  )
+}
+
+export default App
+
